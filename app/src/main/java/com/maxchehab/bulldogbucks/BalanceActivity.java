db2edb21101d2c4421c3f9a33d1c7b91056b0516
maxchehab <maxchehab@gmail.com>
2017-08-31 16:02:20 -0700

    Truncation for transaction location names

--- app/src/main/java/com/maxchehab/bulldogbucks/BalanceActivity.java
+++ app/src/main/java/com/maxchehab/bulldogbucks/BalanceActivity.java
@@ -346,7 +346,7 @@ public class BalanceActivity extends AppCompatActivity {
                             ImageView down = (ImageView) transactionView.findViewById(R.id.down);
 
                             amount.setText("$" + decimalFormat.format(transaction.getAmount()));
-                            location.setText(transaction.getLocation());
+                            location.setText(ellipsize(transaction.getLocation(),15));
 
                             if(transaction.getType().equalsIgnoreCase("DEPOSIT")){
                                 amount.setTextColor(Color.parseColor("#719862"));
@@ -355,7 +355,6 @@ public class BalanceActivity extends AppCompatActivity {
                                 amount.setTextColor(Color.parseColor("#b70101"));
                                 down.setVisibility(View.VISIBLE);
                             }
-
 
                             _transactionsParent.addView(transactionView);
                         }
@@ -382,5 +381,39 @@ public class BalanceActivity extends AppCompatActivity {
             }
         }).execute(new Credential(userID,pin));
         //swipeContainer.setRefreshing(false);
+    }
+
+    private final static String NON_THIN = "[^iIl1\\.,']";
+
+    private static int textWidth(String str) {
+        return (int) (str.length() - str.replaceAll(NON_THIN, "").length() / 2);
+    }
+
+    public static String ellipsize(String text, int max) {
+
+        if (textWidth(text) <= max)
+            return text;
+
+        // Start by chopping off at the word before max
+        // This is an over-approximation due to thin-characters...
+        int end = text.lastIndexOf(' ', max - 3);
+
+        // Just one long word. Chop it off.
+        if (end == -1)
+            return text.substring(0, max-3) + "...";
+
+        // Step forward as long as textWidth allows.
+        int newEnd = end;
+        do {
+            end = newEnd;
+            newEnd = text.indexOf(' ', end + 1);
+
+            // No more spaces.
+            if (newEnd == -1)
+                newEnd = text.length();
+
+        } while (textWidth(text.substring(0, newEnd) + "...") < max);
+
+        return text.substring(0, end) + "...";
     }
 }
